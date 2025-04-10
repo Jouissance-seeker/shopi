e99487375ccb48333dc33f68ed80f0c0fe931f2b
Jouissance Seeker <051.sys113@gmail.com>
2025-04-10 19:12:43 +0330

    chore: new changes

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,10 +1,13 @@
+import { APIgetCategorySlider } from '@/actions/routes/global/get-category-slider';
 import { BreadCrumb } from '@/components/bread-crumb';
 import { CheckboxAccordions } from '@/containers/routes/explore/checkbox-accordions';
 import { Products } from '@/containers/routes/explore/products';
 import { Sort } from '@/containers/routes/explore/sort';
 import { CategorySlider } from '@/containers/routes/global/category-slider';
 
-export default function Page() {
+export default async function Page() {
+  const categorySliderData = await APIgetCategorySlider();
+
   return (
     <div className="container relative z-10 flex size-full flex-col gap-3">
       <div className="flex items-center justify-between gap-3">
@@ -15,7 +18,7 @@ export default function Page() {
       <div className="flex lg:gap-3">
         <CheckboxAccordions />
         <div className="grid flex-1 gap-3">
-          <CategorySlider />
+          <CategorySlider data={categorySliderData} />
           <Products />
         </div>
       </div>
